b3e9b79d915e53b153e48d8619566ef79cfd804d
mozhimen <58215586+mozhimen@users.noreply.github.com>
2025-07-11 19:05:39 +0800

    250711

--- ServiceKit/Package.swift
+++ ServiceKit/Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "ISwiftKit.Service",
+    name: "ISwiftKit.ServiceKit",
     platforms: [
-        .macOS(.v12),.iOS(.v13)
+        .macOS(.v10_15),.iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ISwiftKit.Service",
-            targets: ["ISwiftKit.Service"]),
+            name: "ISwiftKit.ServiceKit",
+            targets: ["ISwiftKit.ServiceKit"]),
     ],
     dependencies: [
         .package(name: "SUtilKit.SwiftUI", path: "../../SUtilKit/SwiftUI")
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ISwiftKit.Service",dependencies: ["SUtilKit.SwiftUI"]),
+            name: "ISwiftKit.ServiceKit",dependencies: ["SUtilKit.SwiftUI"]),
 
     ]
 )
